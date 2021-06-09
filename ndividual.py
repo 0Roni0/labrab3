e2c9822b42f5dd3719c07a7c77e122912bb96a06
Your Name <Alex@gmail.com>
2021-06-09 12:19:17 +0300

    izmenen

--- ndividual.py
+++ ndividual.py
@@ -11,6 +11,6 @@ yC=float(input('yC'))
 AB=math.sqrt((xB-xA)**2+(yB-yA)**2)
 BC=math.sqrt((xC-xB)**2+(yC-yB)**2)
 AC=math.sqrt((xC-xA)**2+(yC-yA)**2)
-P=AB+BC+AC
+P=(AB+BC+AC)/2
 S=math.sqrt(P*(P-AB)*(P-BC)*(P-AC))
 print ('Периметр равен',"%.2f"%P, 'Площадь равна',"%.2f"%S)
